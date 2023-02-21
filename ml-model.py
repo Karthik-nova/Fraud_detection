313807b31dd1978ead822deab9b5b5608a7069da
Karthik-nova <karthi12bi@gmail.com>
2023-02-21 11:01:42 +0530

    added 3 ml model to code

--- ml-model.py
+++ ml-model.py
@@ -1,3 +1,14 @@
 
 
 print("adding all the ml related code here")
+
+def model1():
+    print("Adding code for ML model1")
+
+
+def model2():
+    print("Adding code for ML model2")
+
+def model3():
+    print("Adding code for ML model3")
+    
